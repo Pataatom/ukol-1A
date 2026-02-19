741615deaec0755e5e130102095454b18182f28d
Pataatom <122830601+Pataatom@users.noreply.github.com>
2026-02-19 16:42:07 +0100

    Update ukol_1A.py

--- ukol_1A.py
+++ ukol_1A.py
@@ -1,13 +1,15 @@
-#Magician
 # importuje knihovny
 import time
 
 
 # vykona ukon 50x
 for _ in range(50):
-	# Presune efektor z home pozice nad pozici kresleni
+	
+	# Presune efektor na home pozici
 	dType.SetPTPCmd(api, 2, 217, -161, 10, 0, 1)
 	time.sleep(0.5)
+	
+	# Presune efektor nad pozici kresleni
 	dType.SetPTPCmd(api, 2, 280, 15, 10, 0, 1)
 	time.sleep(0.5)
 	
@@ -15,6 +17,7 @@ for _ in range(50):
 	dType.SetPTPCmd(api, 2, 280, 15, -64, 0, 1)
 	time.sleep(0.5)
 	dType.SetPTPCmd(api, 2, 280, 15, 10, 0, 1)
+
 
 
 
