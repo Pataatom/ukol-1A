deef7d5956c448284beb7513932a5f764df159cc
Pataatom <122830601+Pataatom@users.noreply.github.com>
2026-02-19 16:39:53 +0100

    Add files via upload

---
+++ ukol_1A.py
@@ -0,0 +1,20 @@
+#Magician
+# importuje knihovny
+import time
+
+
+# vykona ukon 50x
+for _ in range(50):
+	# Presune efektor z home pozice nad pozici kresleni
+	dType.SetPTPCmd(api, 2, 217, -161, 10, 0, 1)
+	time.sleep(0.5)
+	dType.SetPTPCmd(api, 2, 280, 15, 10, 0, 1)
+	time.sleep(0.5)
+	
+	# Nakresli tecku 
+	dType.SetPTPCmd(api, 2, 280, 15, -64, 0, 1)
+	time.sleep(0.5)
+	dType.SetPTPCmd(api, 2, 280, 15, 10, 0, 1)
+
+
+
